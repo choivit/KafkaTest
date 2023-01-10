b414e11b9ea57d2f8f3f1a0f5f911bdb021da25a
choivit <topvit5@hanmail.net>
2023-01-10 11:11:14 +0900

    edit topic

--- src/main/java/com/example/kafkagroovy/kafkaTest/KafkaConsumer.java
+++ src/main/java/com/example/kafkagroovy/kafkaTest/KafkaConsumer.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 @Service
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "fruit", groupId = "choi")
+    @KafkaListener(topics = "choi", groupId = "choi")
     public void consume(String message) throws IOException {
         System.out.println(String.format("Consumed message : %s", message));
     }
--- src/main/java/com/example/kafkagroovy/kafkaTest/KafkaProducer.java
+++ src/main/java/com/example/kafkagroovy/kafkaTest/KafkaProducer.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaProducer {
-    private static final String TOPIC = "fruit";
+    private static final String TOPIC = "choi";
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     @Autowired
